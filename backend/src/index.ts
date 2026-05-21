f4c9c47dae5d8b56a6e0da5b39dd999f927d8a22
vivekcore <vivek.inz8@gmail.com>
2026-05-21 17:38:55 +0530

    Frontend url missmatch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ const startServer = async () => {
 
     app.use(
       cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
       }),
     );
