38560d35794d171230f70aa104aada3da6ef6d36
vivekcore <vivek.inz8@gmail.com>
2026-05-31 15:04:08 +0530

    fix smpt error

--- backend/src/utils/sendVerificationMail.ts
+++ backend/src/utils/sendVerificationMail.ts
@@ -1,15 +1,21 @@
-
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import ApiError from "./apiError.js";
 dotenv.config();
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service:'gmail',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
+transporter.verify((err) => {
+  if (err) console.error('Resend SMTP Error:', err);
+  else console.log(' Resend SMTP Ready');
+});
 interface IsendEmail {
   to: string;
   url?: string;
@@ -18,13 +24,14 @@ interface IsendEmail {
   tag?: "verify" | "forget" | "existinguser";
 }
 
-export async function sendEmail({ to, url, subject, text,tag }: IsendEmail) {
-  if (tag === "verify") {
-    await transporter.sendMail({
-      from: 'Stashly',
-      to,
-      subject,
-      html: `
+export async function sendEmail({ to, url, subject, text, tag }: IsendEmail) {
+  try {
+    if (tag === "verify") {
+      await transporter.sendMail({
+        from: `"Stashly"`,
+        to,
+        subject,
+        html: `
       <h2>Email Verification</h2>
       <p>Click the button below to verify your email:</p>
       <a href="${url}"
@@ -39,14 +46,14 @@ export async function sendEmail({ to, url, subject, text,tag }: IsendEmail) {
          Verify Email
       </a>
     `,
-    });
-  }
-  if(tag === 'forget'){
-    await transporter.sendMail({
-      from:"Stashly",
-      to,
-      subject,
-      html: `
+      });
+    }
+    if (tag === "forget") {
+      await transporter.sendMail({
+        from: `"Stashly" `,
+        to,
+        subject,
+        html: `
       <h2>Password Reset<h2>
       <p>click the button below to reset your password:</p>
       <a href="${url}"
@@ -61,15 +68,18 @@ export async function sendEmail({ to, url, subject, text,tag }: IsendEmail) {
          Reset password
       </a>
 
-      `
-    })
-  }
-  if (tag === "existinguser") {
-    await transporter.sendMail({
-      from: "Stashly",
-      to,
-      subject,
-      text,
-    });
+      `,
+      });
+    }
+    if (tag === "existinguser") {
+      await transporter.sendMail({
+        from: `"Stashly"`,
+        to,
+        subject,
+        text,
+      });
+    }
+  } catch (error) {
+    throw new ApiError(400, "Email delivery failed");
   }
 }
